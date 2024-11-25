d3894e7517c66b23108cb89ea6bae6c33ffbf06f
MichelleF <deshanfernando67@gmail.com>
2024-11-25 22:54:48 +0530

    Update TicketingSystemApplication.java

--- ticketing_system/src/main/java/com/oop/ticketing_system/TicketingSystemApplication.java
+++ ticketing_system/src/main/java/com/oop/ticketing_system/TicketingSystemApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TicketingSystemApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TicketingSystemApplication.class, args);
+	System.out.println("gsesgs")
 	}
 
 }
