c6851c0cdb3ebdd731202d455e3b39a4dfc0f3d5
MichelleF <deshanfernando67@gmail.com>
2024-11-25 23:09:35 +0530

    new

--- ticketing_system/src/main/java/com/oop/ticketing_system/TicketingSystemApplication.java
+++ ticketing_system/src/main/java/com/oop/ticketing_system/TicketingSystemApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TicketingSystemApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(TicketingSystemApplication.class, args);
-	System.out.println("gsesgs")
 	}
 
 }
